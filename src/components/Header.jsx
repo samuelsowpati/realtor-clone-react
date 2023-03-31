bcd55b75f208958c2d3748f617c17d305f09721d
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2023-03-31 18:43:26 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,7 +53,7 @@ export default function Header() {
     <div className="bg-white border-b shadow-sm sticky top-0 z-40">
         <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
             <div>
-                <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className="h-5 cursor-pointer" onClick={()=>navigate("/")}/>
+                <img src="https://github.com/deephunt3r/d/blob/main/Screenshot%20from%202023-03-31%2018-35-31.png" alt="logo" className="h-5 cursor-pointer" onClick={()=>navigate("/")}/>
             </div>
             <div>
                 <ul className="flex space-x-9">
